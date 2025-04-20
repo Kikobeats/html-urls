2bfc901326239a8a24e1f8ae439a63b08e1684fc
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-04-20 14:26:25 +0200

    chore: updated tags

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ const TAGS = {
   data: ['object'],
   formaction: ['button', 'input'],
   href: ['a', 'area', 'base', 'link'],
+  icon: ['menuitem'],
+  manifest: ['html'],
   ping: ['a', 'area'],
   poster: ['video'],
   src: ['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video']
